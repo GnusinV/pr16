39ce01ea95b7b61102f373183900a9ab9e7153de
Vladislav Gnusin <ashifin@me.com>
2022-11-19 16:59:44 +0700

    Initial Commit

---
+++ pr16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pr16
+//
+//  Created by Владислав Гнусин on 19.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
